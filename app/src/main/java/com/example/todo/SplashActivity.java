4d410a12755cb009530fd14560180829cb8c9e15
Silvia Asmat <silvia.asmat@g.austincc.edu>
2023-07-21 13:57:20 -0500

    Added delay for splash activity

--- app/src/main/java/com/example/todo/SplashActivity.java
+++ app/src/main/java/com/example/todo/SplashActivity.java
@@ -3,6 +3,7 @@ package com.example.todo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        },1000);
     }
 }
